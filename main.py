92c35f133e53ad75ac265800fd9cf0869858b90e
wesblake@proton.me <wesblake@proton.me>
2026-06-24 11:39:57 -0700

    Add: notes for future additions.

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ import pyautogui as pag
 from onbaser import *
 from remaining_files import remaining_files
 
+# TODO: logger to json.
+# dir name {
+#   date [
+#       full file name
+#]
+#}
 
+
+# try catch
 cfg = configparser.ConfigParser()
 cfg.read('.env')
 if cfg:
@@ -16,43 +24,61 @@ if cfg:
     input_path = Path(cfg['file_store']['drop_box_path'])
 else:
     raise FileNotFoundError("Config file not found: .env")
+# end try catch
 
+# two if statements
 if not onbase_path.is_dir() or not input_path.is_dir():
     raise FileNotFoundError(
         f"OnBase path not found: {onbase_path}"
     )
+# end two if statements
 
+# two if statements
+# Add over special options
 if not os.path.isfile(primary_id_img) and not os.path.isfile(event_date_img):
     raise FileNotFoundError(
         f"Image file not found: {primary_id_img} or {event_date_img}"
     )
+# end two if statements
 
+# Change for to while loop
 starter(onbase_path)
 for i in range(len(os.listdir(str(onbase_path)))):
+# end change to while loop
     doc_type_name = copy_file_name()
+    # Check still works?
     if doc_type_name.startswith("1."):
         pag.press('down')
         continue
+    # end Check still works?
     length = len(os.listdir(str(onbase_path / doc_type_name)))
     if length == 0:
         pag.press('down')
+        # restart loop maybe?
+        # starter(onbase_path)
         if doc_type_name == "zkill":
             break
+        # end restart loop maybe?
     else:
         pag.press('enter')
         import_doc_box(doc_type_name)
         for file in range(length):
+            # info to file_name
             info = file_dragger(onbase_path, doc_type_name, input_path)
             if info is None:
                 continue
             import_boxes(info)
             keyword_boxes(primary_id_img, event_date_img, info)
+            # end info to file_name
             complete()
         # return ..\\
+        # focus file explorer
+        # press alt + up
         go_to_parent = (1084, 63)
         time.sleep(0.5)
         pag.moveTo(go_to_parent, duration=0.3)
         pag.click()
+        # end focus file explorer
         pag.press('down')
 
 remaining_files(input_path)
--- onbaser.py
+++ onbaser.py
@@ -7,15 +7,25 @@ import pyautogui as pag
 
 
 def starter(base_dir_path: Path) -> None:
+    """File explorer function, starter"""
     first_dir = (1253, 208)
+    # start new search bar
+    # focus file explorer
+    # press ctrl + l
     explor_search_bar = (1677, 63)
     pag.moveTo(explor_search_bar, duration=0.1)
     pag.click()
+    # end start new search bar
     pag.write(str(base_dir_path), interval=0.03)
     pag.press('enter')
+    # remove
     pag.press('esc')
+    # end remove
+    # focus dir list
+    # press home key
     pag.moveTo(first_dir, duration=0.1)
     pag.leftClick()
+    # end focus dir list
 
 
 def cancel_box() -> None:
@@ -46,32 +56,45 @@ def import_doc_box(document_type_name: str) -> None:
     time.sleep(1)
 
 
-def file_dragger(onbase_path: Path,
-                 doc_type_name: str,
-                 input_path: Path) -> list | None:
+def file_dragger(
+    onbase_path: Path,
+    doc_type_name: str,
+    input_path: Path
+    ) -> list | None:
+    # ocr
     first_file = (1192, 175)
+    # calc center
     file_drop = (610, 525)
+    # calc center
+    # end ocr
     pag.moveTo(first_file, duration=0.1)
     pag.leftClick()
     file_name = copy_file_name()
+    # make file dates only a number example 20260526
+    # make split only '-'
     result = file_name.split(' - ')
-    try:
-        datetime.fromisoformat(result[1])
-    except ValueError:
-        return _mover(onbase_path, doc_type_name, input_path, file_name)
     if len(result) < 2:
         return _mover(onbase_path, doc_type_name, input_path, file_name)
     if not result[0].isdigit():
         return _mover(onbase_path, doc_type_name, input_path, file_name)
+    try:
+        datetime.fromisoformat(result[1])
+    except ValueError:
+        return _mover(onbase_path, doc_type_name, input_path, file_name)
+    # end make file dates
     pag.dragTo(file_drop, duration=0.2)
+    # ocr?
     time.sleep(3)
+    # end ocr?
     return result
 
 
-def _mover(onbase_path: Path,
-           doc_type_name: str,
-           input_path: Path,
-           file_name: str) -> None:
+def _mover(
+    onbase_path: Path,
+    doc_type_name: str,
+    input_path: Path,
+    file_name: str
+    ) -> None:
     src = onbase_path / doc_type_name / file_name
     dst = input_path / file_name
     if not src.exists():
@@ -89,8 +112,10 @@ def _mover(onbase_path: Path,
 
 
 def import_boxes(info: list) -> None:
+    # ocr
     document_date = (174, 311)
     pag.moveTo(document_date, duration=0.1)
+    # end ocr
     pag.click()
     time.sleep(0.2)
     pag.hotkey('ctrl','a')
@@ -100,26 +125,37 @@ def import_boxes(info: list) -> None:
     pag.press('enter')
 
 
-def keyword_boxes(id_img_path: str,
-                  event_img_path: str,
-                  info: list) -> None:
+def keyword_boxes(
+    id_img_path: str,
+    event_img_path: str,
+    info: list
+    ) -> None:
     try:
+        # ocr
         left, top, *_ = pag.locateOnScreen(event_img_path, confidence=0.9)
+        #x, y = pag.locateCenterOnScreen(event_img_path, confidence=0.9)
         pag.moveTo((left + 32, top + 32), duration=0.2)
+        # end ocr
         pag.click()
         pag.write(info[1], interval=0.03)
         pag.press('tab')
     except:
         pass
+    # ocr
     left, top, *_ = pag.locateOnScreen(id_img_path, confidence=0.9)
     pag.moveTo((left + 30, top + 27), duration=0.2)
+    # end ocr
     pag.click()
     pag.write(info[0], interval=0.03)
     pag.press('tab')
 
 
 def complete() -> None:
+    # ocr
     import_button = (88, 994)
     pag.moveTo(import_button, duration=0.1)
+    # end ocr
     pag.click()
+    # ocr wait on uploading
     time.sleep(4)
+    # end ocr wait on uploading
