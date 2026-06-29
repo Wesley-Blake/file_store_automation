125a2def34138bf9e1b2296f3a80fdf841dbe40e
wesblake@proton.me <wesblake@proton.me>
2026-06-29 15:40:29 -0700

    Refactor: implementing new refeactor.

--- main.py
+++ main.py
@@ -1,85 +1,55 @@
-import os
+"""Main loop to take files from file explorer to file storage software."""
 from pathlib import Path
-import time
 import configparser
 import pyautogui as pag
-from onbaser import *
+from onbaser import FileExplorer
+from onbaser import FileStore
 from remaining_files import remaining_files
 
-# TODO: logger to json.
-# dir name {
-#   date [
-#       full file name
-#]
-#}
+# TODO: logger
+# Date&time: Doc name: filename
 
-
-# try catch
 cfg = configparser.ConfigParser()
-cfg.read('.env')
-if cfg:
-    primary_id_img = cfg['file_store']['p_id_img']
-    event_date_img = cfg['file_store']['event_date_img']
-    import_button_img = cfg['file_store']['import_button_img']
-    onbase_path = Path(cfg['file_store']['onbase_path'])
-    input_path = Path(cfg['file_store']['drop_box_path'])
-else:
-    raise FileNotFoundError("Config file not found: .env")
-# end try catch
-
-# two if statements
-if not onbase_path.is_dir() or not input_path.is_dir():
-    raise FileNotFoundError(
-        f"OnBase path not found: {onbase_path}"
-    )
-# end two if statements
-
-# two if statements
-# Add over special options
-if not os.path.isfile(primary_id_img) and not os.path.isfile(event_date_img):
-    raise FileNotFoundError(
-        f"Image file not found: {primary_id_img} or {event_date_img}"
-    )
-# end two if statements
+# NOTE: if this fails, the program should fail.
+if not cfg.read('.env'):
+    raise FileNotFoundError("Either the file is empty or doesn't exist.")
+try:
+    # File explorer object
+    file_explorer_path = Path(cfg['file_store']['file_explorer_path'])
+    drop_box_path = Path(cfg['file_store']['drop_box_path'])
+    first_file_img = Path(cfg['file_store']['first_file_img'])
+    # File storage object
+    #import_button_img = cfg['file_store']['import_button_img']
+    primary_id_img = Path(cfg['file_store']['p_id_img'])
+    event_date_img = Path(cfg['file_store']['event_date_img'])
+except KeyError as exc:
+    raise KeyError("It looks like you are missing one or more keys.") from exc
 
 # Change for to while loop
-starter(onbase_path)
-for i in range(len(os.listdir(str(onbase_path)))):
-# end change to while loop
-    doc_type_name = copy_file_name()
-    # Check still works?
-    if doc_type_name.startswith("1."):
-        pag.press('down')
-        continue
-    # end Check still works?
-    length = len(os.listdir(str(onbase_path / doc_type_name)))
-    if length == 0:
-        pag.press('down')
-        # restart loop maybe?
-        # starter(onbase_path)
-        if doc_type_name == "zkill":
-            break
-        # end restart loop maybe?
-    else:
+# TODO: instantiate both objects.
+# TODO: both starters
+# We aren't going to use a variable from a for loop.
+while True:
+    # TODO: Check if first copy is dropbox: continue
+    # TODO: get doc_type_name
+    current_dir = onbase_path / copy_file_name()
+    if next(current_dir.iterdir(), None):
         pag.press('enter')
-        import_doc_box(doc_type_name)
-        for file in range(length):
-            # info to file_name
-            info = file_dragger(onbase_path, doc_type_name, input_path)
-            if info is None:
+        # TODO: do the import box
+        import_doc_box(current_dir.name)
+        for _ in current_dir.iterdir():
+            # TODO: add dragger
+            if info := file_dragger(current_dir.name) is None:
                 continue
+            # TODO: file storage actions.
             import_boxes(info)
-            keyword_boxes(primary_id_img, event_date_img, info)
-            # end info to file_name
-            complete()
-        # return ..\\
-        # focus file explorer
-        # press alt + up
+            keyword_boxes(info)
+        # TODO: go to parrent
         go_to_parent = (1084, 63)
-        time.sleep(0.5)
-        pag.moveTo(go_to_parent, duration=0.3)
-        pag.click()
-        # end focus file explorer
-        pag.press('down')
+        pag.click(go_to_parent, duration=0.3)
+    pag.press('down')
+    if current_dir.name == "zkill":
+        break
 
+# TODO: update path
 remaining_files(input_path)
--- onbaser.py
+++ onbaser.py
@@ -1,12 +1,20 @@
+"""pyautogui logic to navigate File explorer or file storage software."""
 import time
 import shutil
 from pathlib import Path
 from datetime import datetime
 import pyperclip
 import pyautogui as pag
+from pyautogui import ImageNotFoundException
 
-class file_explorer():
-    def __init__(self, file_store_path: Path, drop_box_path: Path, first_file_img: Path):
+class FileExplorer():
+    """Actions to navigate File Explorer."""
+    def __init__(
+        self,
+        file_store_path: Path,
+        drop_box_path: Path,
+        first_file_img: Path
+    ):
         if not all(
             file_store_path.exists(),
             drop_box_path.exists(),
@@ -15,38 +23,52 @@ class file_explorer():
             raise FileNotFoundError("Args aren't proper paths.")
         self.file_store_path = file_store_path
         self.drop_box_path = drop_box_path
-        self.first_file_img = first_file_img
-        self.x, self.y = pag.size()
-        x, y , w, h= pag.locateOnScreen(str(self.first_file_img), confidence=0.7, region=(int(self.x*0.5),0,self.x,int(self.y*0.5)))
-        self.first_tuple = (x + w * 0.5, y + h * 1.25)
+        size_x, size_y = pag.size()
+        x, y , w, h = pag.locateOnScreen(
+            str(first_file_img),
+            confidence=0.7,
+            # Narrow search to right top screen
+            # Requires integers, doesn't accept float.
+            region=(int(size_x*0.5),0,size_x,int(size_y*0.5))
+        )
+        self.first_file = (x + w * 0.5, y + h * 1.25)
+        self.file_drop = (size_x * 0.35, size_y * 0.5)
+        self.focus_first_file = (size_x * 0.75, size_y * 0.75)
 
-    def _firstFile(self):
-        pag.click(self.x * 0.75, self.y * 0.75)
+
+    def _focus_first_file(self) -> None:
+        pag.click(self.focus_first_file)
         pag.press('home')
-        # Becuase home doesn't always set teh focus.
+        # Becuase home doesn't always set the focus.
         pag.press('PgUp')
 
     def starter(self) -> None:
         """File explorer function, starter"""
-        self._firstFile()
+        self._focus_first_file()
         pag.hotkey('ctrl','l')
         #pag.write(str(self.file_store_path), interval=0.03)
         pag.write(str(self.file_store_path))
         pag.press('enter')
-        self._firstFile()
+        self._focus_first_file()
 
     def copy_file_name(self) -> str:
-        self._firstFile()
+        """Copy the name of the first file/dir in file explorer."""
+        self._focus_first_file()
         pag.press('f2')
+        # File Explorer is slow, gives it a moment to do the thing.
         time.sleep(0.3)
+        # We want to the fix extention just incase it isn't valid.
         pag.hotkey('ctrl','a')
         pag.hotkey('ctrl','c')
         result = pyperclip.paste()
         pag.press('esc')
         return result
 
-    def _insert_day_time(file_name):
-        #(file_name[:file_name.rfind('.')] + datetime.now().strftime("-%Y%m%d%H%M%S") + file_name[file_name.rfind('.'):])
+    def _insert_day_time(self, file_name: str) -> str:
+        """
+        If file doesn't match expected result and the file already exists in dropbox,
+        this will add date&time stamp to the end of the file name before the extention.
+        """
         root = file_name[:file_name.rfind('.')]
         date_time = datetime.now().strftime("-%Y%m%d%H%M%S")
         extention = file_name[file_name.rfind('.'):]
@@ -57,7 +79,7 @@ class file_explorer():
         dst = self.drop_box_path / file_name
         if not src.exists():
             raise FileExistsError(f"Source Doesn't Exist: {src}")
-        elif dst.exists():
+        if dst.exists():
             shutil.move(
                 src,
                 self.drop_box_path / self._insert_day_time(file_name),
@@ -68,6 +90,7 @@ class file_explorer():
         time.sleep(1)
 
     def file_dragger(self, doc_type_name: str) -> list | None:
+        """Move first file to files storage application."""
         # Brings focus to first file in explorer
         file_name = self.copy_file_name()
         # TODO: detect file date format transform to 20260525
@@ -82,54 +105,86 @@ class file_explorer():
         except ValueError:
             return self._mover(doc_type_name, file_name)
         # Move mouse to first file.
-        pag.moveTo(self.first_tuple)
-        file_drop = (self.x * 0.35, self.y * 0.5)
-        pag.dragTo(file_drop, duration=0.3)
+        pag.moveTo(self.first_file)
+        pag.dragTo(self.file_drop, duration=0.3)
         # ocr?
         time.sleep(2)
         # end ocr?
         return result
 
 
-class file_store():
-    def __init__(import_button_img: Path, primary_id_img: Path, event_date_img: Path):
-        if not all(
-            import_button_img.exists(),
-            primary_id_img.exists(),
-            event_date_img.exists()
-        ):
-            raise FileNotFoundError("Arguments not proper path.")
-        self.import_button_img = import_button_img
-        self.primary_id_img = primary_id_img
+class FileStore():
+    """Actions to navigate File Storage software."""
+    def __init__(
+        self,
+        #import_button_img: Path,
+        primary_id_img: Path,
+        event_date_img: Path
+    ):
+        size_x, size_y = pag.size()
+        # Region for fields entry.
+        # TODO: future, not self.region but region.
+        self.region = (0,0, size_x*0.5, size_y)
+        # first import button and second in complete.
+        #self.import_button_img = import_button_img
+        #self.import_button_img = import_button_img
+        self.primary_id = pag.locateCenterOnScreen(
+            primary_id_img,
+            confidence=0.9,
+            region=self.region
+        )
+        # TODO: future marker for each doc type.
         self.event_date_img = event_date_img
-        self.x, self.y = pag.size()
-        self.region = (0,0, self.x*0.5, self.y)
+    def starter(
+            self,
+            import_button_img: Path,
+            import_button_check_img: Path
+        ) -> None:
+        """Starter for file storage software"""
+        #try:
+        #    center = locateCenterOnScreen(
+        #        str(import_button_img),
+        #        confidence=0.7,
+        #        region=self.region,
+        #    )
+        #    pag.click(center)
+        #try:
+        #    center = locateCenterOnScreen(
+        #        str(import_button_check_img),
+        #        confidence=0.7,
+        #        region=self.region,
+        #    )
+        pass
 
     def _cancel_box(self) -> None:
         #time.sleep(1)
         # TODO: ocr
         cancel_button = (175, 80)
-        pag.moveTo(cancel_button, duration=0.1)
+        pag.click(cancel_button, duration=0.1)
         # end ocr
-        pag.click()
+        #pag.click()
+        # Give the program a moment to catch up.
         time.sleep(1)
 
     def import_doc_box(self, document_type_name: str) -> None:
+        """
+        Set document type box based on folder name from FileExplorer.copy_file_name().
+        """
         # ocr
         self._cancel_box()
         document_type = (227, 232)
-        pag.moveTo(document_type, duration=0.1)
-        pag.click()
+        pag.click(document_type, duration=0.1)
+        #pag.click()
         # end ocr
         pag.write(document_type_name, interval=0.05)
         pag.press('enter')
         #time.sleep(1)
 
-    def date_box(self, info: list) -> None:
+    def _date_box(self, info: list) -> None:
         # TODO: ocr
         document_date = (174, 311)
-        pag.moveTo(document_date, duration=0.1)
-        pag.click()
+        pag.click(document_date, duration=0.1)
+        #pag.click()
         # end ocr
         time.sleep(0.1)
         pag.hotkey('ctrl','a')
@@ -138,48 +193,32 @@ class file_store():
         pag.write(info[1], interval=0.03)
         pag.press('enter')
 
-    def keyword_boxes(self, info: list) -> None:
-        try:
-            # ocr
-            center = pag.locateCenterOnScreen(self.event_date_img, confidence=0.9, region=self.region)
-            #x, y = pag.locateCenterOnScreen(event_img_path, confidence=0.9)
-            #pag.moveTo(center, duration=0.2)
-            # end ocr
-            pag.click(center, duration=0.2)
-            pag.write(info[1], interval=0.03)
-            pag.press('tab')
-        except:
-            pass
-        # ocr
-        center = pag.locateCenterOnScreen(id_img_path, confidence=0.9, region=self.region)
-        #pag.moveTo(center, duration=0.2)
-        # end ocr
-        pag.click(center, duration=0.2)
-        pag.write(info[0], interval=0.03)
-        pag.press('tab')
-
-    def complete() -> None:
+    def _complete(self) -> None:
         # ocr
         import_button = (88, 994)
-        pag.moveTo(import_button, duration=0.1)
+        pag.click(import_button, duration=0.1)
         # end ocr
-        pag.click()
+        #pag.click()
         # ocr wait on uploading
         time.sleep(4)
         # end ocr wait on uploading
 
-if __name__ == '__main__':
-    import configparser
-    cfg = configparser.ConfigParser()
-    cfg.read('.env')
-    if cfg:
-        primary_id_img = cfg['file_store']['p_id_img']
-        event_date_img = cfg['file_store']['event_date_img']
-        import_button_img = cfg['file_store']['import_button_img']
-        onbase_path = Path(cfg['file_store']['onbase_path'])
-        input_path = Path(cfg['file_store']['drop_box_path'])
-        first_file_img = Path(cfg['file_store']['first_file_img'])
-    else:
-        raise FileNotFoundError("Config file not found: .env")
-    file_e = file_explorer(onbase_path, input_path, first_file_img)
-    file_e.file_dragger("SF Payroll Timesheet")
+    def keyword_boxes(self, info: list) -> None:
+        """Insert info from file name to field boxes."""
+        self._date_box(info)
+        try:
+            center = pag.locateCenterOnScreen(
+                self.event_date_img,
+                confidence=0.9,
+                region=self.region
+            )
+            pag.click(center, duration=0.2)
+            pag.write(info[1], interval=0.03)
+            pag.press('tab')
+        except ImageNotFoundException:
+            print("Could not locate event date.")
+        pag.click(center, duration=0.2)
+        pag.write(info[0], interval=0.03)
+        pag.press('tab')
+        # TODO: copy employee name field and check if len>0
+        self._complete()
