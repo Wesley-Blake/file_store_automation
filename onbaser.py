c626b52ecaba1b666d6d21bace74cc7302da3800
wesblake@proton.me <wesblake@proton.me>
2026-07-24 16:50:21 -0700

    Final update to logging format to include directory names.

--- onbaser.py
+++ onbaser.py
@@ -13,36 +13,37 @@ from pyautogui import ImageNotFoundException
 class FileExplorer:
     """Actions to navigate File Explorer."""
 
-    def __init__(
-        self, file_store_path: Path, drop_box_path: Path, first_file_img: Path
-    ):
-        self._file_store_path = file_store_path
-        self._drop_box_path = drop_box_path
+    def __init__(self, root: str, drop_box_path: str, file_drag: str):
+        self._root = root
+        self._drop_box_path = Path(drop_box_path)
         size_x, size_y = pag.size()
-        self._focus_first_file = (size_x * 0.75, size_y * 0.75)
+        self._focus_explorer = (size_x * 0.75, size_y * 0.75)
         self._file_drop = (size_x * 0.35, size_y * 0.5)
+        temp = pag.locateOnScreen(
+            file_drag,
+            confidence=0.7,
+            region=(int(size_x * 0.5), 0, size_x, int(size_y * 0.3)),
+        )
+        self._file_drag_region = (int(temp.left * 1.25), int(temp.top * 1.9))
         self._start()
 
-    def _focus_first_file(self) -> None:
-        pag.click(self._focus_first_file)
+    def focus_explorer_file(self) -> None:
+        pag.click(self._focus_explorer)
         pag.press("home")
         # Becuase home doesn't always set the focus.
         pag.press("PgUp")
 
     def _start(self) -> None:
         """File explorer function, starter"""
-        self._focus_first_file()
-        pag.hotkey("ctrl", "l")
-        pag.write(str(self._file_store_path))
+        pag.click(self._focus_explorer)
+        pag.press("f4")
+        pag.write(str(self._root))
         pag.press("enter")
-        self._focus_first_file()
+        self.focus_explorer_file()
 
     def _copy_item_name(self) -> str:
         """Copy the name of the first file/dir in file explorer."""
-        self._focus_first_file()
         pag.press("f2")
-        # File Explorer is slow, gives it a moment to do the thing.
-        time.sleep(0.3)
         # We want to the fix extention just incase it isn't valid.
         pag.hotkey("ctrl", "a")
         pag.hotkey("ctrl", "c")
@@ -61,7 +62,7 @@ class FileExplorer:
         return root + date_time + extention
 
     def _mover(self, doc_type_name: str, file_name: str) -> None:
-        src = self._file_store_path / doc_type_name / file_name
+        src = Path(self._root) / doc_type_name / file_name
         if not src.exists():
             raise FileExistsError(f"Source Doesn't Exist: {src}")
         shutil.move(
@@ -73,7 +74,6 @@ class FileExplorer:
 
     def file_dragger(self, doc_type_name: str) -> list | None:
         """Move first file to files storage application."""
-        # TODO: log the action
         # Brings focus to first file in explorer
         file_name = self._copy_item_name()
         result = file_name.split("-")
@@ -86,13 +86,13 @@ class FileExplorer:
         except ValueError:
             return self._mover(doc_type_name, file_name)
         # Move mouse to first file.
-        pag.moveTo(self._focus_first_file)
+        pag.moveTo(self._file_drag_region)
         pag.dragTo(self._file_drop, duration=0.3)
         # I want this image to catch the loading bar
         count = 0
-        interval = 0.5
+        interval = 0.1
         save_to = Path.home() / "Downloads"
-        while count < 5:
+        while count < 2:
             pag.screenshot(f"{save_to / f'file_drag{count}.png'}")
             time.sleep(interval)
             count += interval
@@ -104,102 +104,117 @@ class FileStore:
 
     def __init__(
         self,
-        primary_id_img: Path,
-        import_button_img: Path,
-        import_button_check_img: Path,
-        cancel_box_img: Path,
-        doc_type_img: Path,
-        date_box_img: Path,
+        import_start_img: str,
+        import_check_img: str,
+        primary_id_img: str,
+        cancel_box_img: str,
+        doc_type_img: str,
+        date_box_img: str,
     ):
         size_x, size_y = pag.size()
         # Region for fields entry.
         self.info: list | None = None
-        self._region = (0, 0, size_x * 0.5, size_y)
-        self._primary_id = pag.locateCenterOnScreen(
-            primary_id_img, confidence=0.9, region=self._region
-        )
-        # TODO: future marker for each doc type.
-        self._cancel_box = pag.locateCenterOnScreen(
-            cancel_box_img, confidence=0.9, region=(0, 0, size_x * 0.25, size_y * 0.3)
-        )
+        self._region = (0, 0, int(size_x * 0.5), size_y)
+        self._primary_id_img = primary_id_img
+        self._start(import_start_img, import_check_img)
         self._doc_type = pag.locateCenterOnScreen(
-            doc_type_img, confidence=0.9, region=(0, 0, size_x * 0.25, size_y * 0.25)
+            doc_type_img,
+            confidence=0.9,
+            region=(0, 0, int(size_x * 0.25), int(size_y * 0.25)),
         )
-        self._date_box = pag.locateCenterOnScreen(
-            date_box_img, confidence=0.9, region=(0, 0, size_x * 0.25, size_y * 0.4)
+        self.cancel_box = pag.locateCenterOnScreen(
+            cancel_box_img,
+            confidence=0.9,
+            region=(0, 0, int(size_x * 0.25), int(size_y * 0.3)),
+        )
+        self._date_field = pag.locateCenterOnScreen(
+            date_box_img,
+            confidence=0.9,
+            region=(0, 0, int(size_x * 0.25), int(size_y * 0.4)),
         )
         # Lazy for now.
-        self._complete = (size_x * 0.05, size_y * 0.92)
+        self._complete = (int(size_x * 0.05), int(size_y * 0.92))
         # End lazy approach for locating complete button.
-        self._start(import_button_img, import_button_check_img)
 
-    def _start(self, import_loader_img: Path, import_check_img: Path) -> None:
+    def _start(self, import_start_img: str, import_check_img: str) -> None:
         """Starter for file storage software"""
         size_x, size_y = pag.size()
         try:
             center = pag.locateCenterOnScreen(
-                str(import_loader_img),
+                str(import_start_img),
                 confidence=0.7,
-                region=(0, 0, size_x * 0.5, size_y * 0.3),
+                region=(0, 0, int(size_x * 0.5), int(size_y * 0.3)),
             )
             pag.click(center)
+            time.sleep(1)
         except ImageNotFoundException:
             raise ImageNotFoundException(
-                f"Could not locate import button image: {import_loader_img}"
+                f"Could not locate import button image: {import_start_img}"
             )
         try:
+            x, y = pag.size()
             pag.locateCenterOnScreen(
-                str(import_check_img),
+                import_check_img,
                 confidence=0.7,
-                region=self._region,
+                region=(0, int(y * 0.8), int(x * 0.25), y),
             )
         except ImageNotFoundException:
             raise ImageNotFoundException(
                 f"Could not locate import button check image: {import_check_img}"
             )
 
-    def _cancel_box(self) -> None:
-        pag.click(self._cancel_box, duration=0.1)
+    def _cancel(self) -> None:
+        time.sleep(0.5)
+        pag.click(self.cancel_box)
         # Give the program a moment to catch up.
-        time.sleep(1)
+        time.sleep(0.5)
 
     def import_doc_box(self, document_type_name: str) -> None:
         """
-        Set document type box based on folder name from FileExplorer.copy_item_name().
+        Set document type box based on folder name from FileExplorer._copy_item_name().
         """
-        self._cancel_box()
-        pag.click(self._doc_type, duration=0.1)
-        pag.write(document_type_name, interval=0.05)
+        self._cancel()
+        pag.click(self._doc_type)
+        pag.write(document_type_name)
         pag.press("enter")
 
     def _date_box(self) -> None:
-        pag.click(self._date_box, duration=0.1)
+        pag.click(self._date_field)
         pag.hotkey("ctrl", "a")
         time.sleep(0.1)
-        pag.write(self.info[1], interval=0.03)
+        pag.write(self.info[1])
         pag.press("tab")
         pag.press("tab")
-
-    def _complete(self) -> None:
-        pag.click(self._complete, duration=0.1)
-        count = 0
-        interval = 0.5
-        save_to = Path.home() / "Downloads"
-        while count < 5:
-            pag.screenshot(f"{save_to / f'complete_button{count}.png'}")
-            time.sleep(interval)
-            count += interval
 
     def keyword_boxes(self) -> None:
         """Insert info from file name to field boxes."""
         self._date_box()
         pag.hotkey("ctrl", "a")
-        pyperclip.copy()
+        pag.hotkey("ctrl", "c")
         if pyperclip.paste() == "    -  -  ":
-            pag.write(self.info[1], interval=0.03)
+            pag.write(self.info[1])
             pag.press("tab")
-        pag.click(self._primary_id, duration=0.2)
-        pag.write(self.info[0], interval=0.03)
-        pag.press("tab")
-        # TODO: copy employee name field and check if len>0
-        self._complete()
+        try:
+            x, y = pag.size()
+            self._primary_id = pag.locateCenterOnScreen(
+                self._primary_id_img,
+                confidence=0.7,
+                region=(0, 0, int(x * 0.2), int(y * 0.5)),
+            )
+            pag.click(self._primary_id)
+            pag.write(self.info[0])
+            pag.press("tab")
+        except ImageNotFoundException:
+            raise ImageNotFoundException("Couldn't find primary ID field.")
+
+    def complete(self) -> None:
+        # TODO: log the action
+        # log(self.info)
+        pag.click(self._complete, duration=0.1)
+        count = 0
+        interval = 0.1
+        save_to = Path.home() / "Downloads"
+        while count < 2:
+            pag.screenshot(f"{save_to / f'complete_button{count}.png'}")
+            time.sleep(interval)
+            count += interval
